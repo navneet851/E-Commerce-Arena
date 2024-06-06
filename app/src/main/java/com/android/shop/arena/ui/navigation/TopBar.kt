dcee34b07174bdaee70687b417d91045c8e2708b
navneet <nav700neet@gmail.com>
2024-06-06 18:26:51 +0530

    navigation ui

---
+++ app/src/main/java/com/android/shop/arena/ui/navigation/TopBar.kt
@@ -0,0 +1,59 @@
+package com.android.shop.arena.ui.navigation
+
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.android.shop.arena.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar() {
+
+
+    TopAppBar(
+        modifier = Modifier,
+        title = {
+                Text(text = "")
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color.White,
+            titleContentColor = Color.Black,
+            actionIconContentColor = Color.Black
+        ),
+        navigationIcon = {
+            Image(
+                modifier = Modifier
+                    .padding(12.dp, 0.dp)
+                    .size(43.dp)
+                    .clip(RoundedCornerShape(10.dp))
+                ,
+                painter = painterResource(id = R.drawable.arena_logo),
+                contentScale = ContentScale.Fit,
+                contentDescription = "logo"
+            )
+        },
+        actions = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    modifier = Modifier.size(25.dp),
+                    painter = painterResource(id = R.drawable.notification),
+                    contentDescription = "search")
+            }
+        }
+    )
+}
