d79215e9153f11244dd77d0afeb375d97135d621
navneet <nav700neet@gmail.com>
2024-06-25 14:32:31 +0530

    cart

--- app/src/main/java/com/android/shop/arena/ui/screens/CartScreen.kt
+++ app/src/main/java/com/android/shop/arena/ui/screens/CartScreen.kt
@@ -1,16 +1,22 @@
 package com.android.shop.arena.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -18,43 +24,121 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.android.shop.arena.R
 import com.android.shop.arena.ui.components.ProductCard
+import com.android.shop.arena.ui.theme.CardColor
 
 
 @Preview
 @Composable
 fun CartScreen() {
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
         repeat(3){
             Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween,
                 modifier = Modifier
-                    .padding(16.dp)
-                    .clip(RoundedCornerShape(6.dp))
                     .fillMaxWidth()
-                    .background(Color.White)
-            ) {
-                Box(modifier = Modifier.size(200.dp)){
-                    ProductCard(
-                        modifier = Modifier.size(120.dp),
-                        product = Product(imageResource = R.drawable.gta5_cover, title = "Grand Theft Auto V", description = "description", category = "Action")
-                    )
-                }
+                    .padding(10.dp, 5.dp)
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(CardColor)
+            ){
+                Image(
+                    modifier = Modifier
+                        .size(160.dp)
+                        .padding(10.dp),
+                    painter = painterResource(id = R.drawable.gta5_cover),
+                    contentDescription = "")
 
-
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
+                Column(
+                    verticalArrangement = Arrangement.SpaceBetween
                 ) {
-                    Icon(painter = painterResource(id = R.drawable.baseline_minimize_24), contentDescription = "")
-                    Text(text = "0")
-                    Icon(painter = painterResource(id = R.drawable.baseline_add_24), contentDescription = "")
+                    Text(
+                        text = "Gta 5",
+                        color = Color.Black,
+                        style = MaterialTheme.typography.titleMedium,
+                        textAlign = TextAlign.Left,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp, 8.dp, 0.dp, 0.dp)
+                    )
+                    Text(
+                        text = "Action",
+                        color = Color(0xFF4848AA),
+                        textAlign = TextAlign.Left,
+                        fontSize = 12.sp,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp, 0.dp)
+                    )
+                    Text(
+                        text = "sold by: jfnf",
+                        color = Color.Gray,
+                        textAlign = TextAlign.Left,
+                        fontSize = 12.sp,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp, 0.dp)
+                    )
+
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier
+                            .padding(10.dp)
+                            .width(80.dp)
+                            .height(28.dp)
+                            .clip(RoundedCornerShape(6.dp))
+                            .background(Color(0xFFEAFFEE))
+                            .padding(6.dp)
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.minus),
+                            tint = Color(0xFF66DD7A),
+                            contentDescription = "")
+                        Text(text = "1")
+                        Icon(
+                            painter = painterResource(id = R.drawable.plus),
+                            tint = Color(0xFF66DD7A),
+                            contentDescription = "")
+                    }
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier
+                            .padding(10.dp)
+                            .width(80.dp)
+                            .height(28.dp)
+                    ) {
+                        Text(text = "5,999")
+                        Text(
+                            color = Color.Gray,
+                            text = "7,599",
+                            textDecoration = TextDecoration.LineThrough
+                        )
+
+                    }
+
                 }
             }
 
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
